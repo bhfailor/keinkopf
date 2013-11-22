fce7342f141660f60bd08f13959bff2595c82073
Bruce Failor <bruce.failor@gmail.com>
2013-11-22 15:17:05 +0000

    add provision.sh

---
+++ provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ~/keinkopf/provision.sh
+# should be executed from ~/
+sudo apt-get update
+sudo apt-get install curl
+\curl -L https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
+rvm
+rvm requirements
+rvm install ruby-1.9.3-p448
+rvm use 1.9.3
+rvm use 1.9.3 --default
+rvm rubygems current
+gem install rails
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:ubuntu-mozilla-security/ppa
+sudo apt-get update
+sudo apt-get install firefox
+sudo apt-get install xvfb
+gem install headless
+sudo apt-get install nodejs
+cd keinkopf
+bundle install
+rails s
