af6c27768774898e27c301df6c8184646794d671
Bruce Failor <bruce.failor@gmail.com>
2013-11-22 20:10:05 -0800

    fix create_query_spec so it does not throw error

---
+++ spec/features/create_query_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+feature "Queries" do
+  context "as a user" do
+    scenario "add a query"
+#      visit root_path # this failed miserably - will go back to model spec
+  end
+end
